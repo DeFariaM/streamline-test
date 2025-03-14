7559c64d8e8e69ff9e6313f9c45bb5525f1cb823
MDeFaria <maria.defariasf@gmail.com>
2025-03-13 20:27:49 -0400

    [fix] types on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,19 @@ import PricingSection from "@/components/sections/PricingSection";
 import TestimonialSection from "@/components/sections/TestimonialSection";
 import { useEffect, useState } from "react";
 
+interface Comment {
+  id: number;
+  name: string;
+  body: string;
+  email: string;
+}
+
+interface Testimonial extends Comment {
+  rating: number;
+}
+
 export default function Home() {
-  const [testimonials, setTestimonials] = useState([]);
+  const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -17,10 +28,10 @@ export default function Home() {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/comments",
         );
-        const data = await response.json();
+        const data: Comment[] = await response.json();
         //only take the first 6 testimonials
         const firstTestimonials = data.slice(0, 6);
-        const result = firstTestimonials.map((testimonials) => ({
+        const result: Testimonial[] = firstTestimonials.map((testimonials) => ({
           ...testimonials,
           rating: Math.floor(Math.random() * 2) + 4,
         }));
